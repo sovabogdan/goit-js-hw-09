3ab904d40763ebf6794c7113f0b441ace3fc53ec
Bohdan Sova <sova.bogdan.94@gmail.com>
2022-05-08 20:47:49 +0200

    update_task_01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -10,16 +10,19 @@ refs.stopBtn.addEventListener('click', onStop);
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+refs.stopBtn.disabled = true;
 
 function onStart() {
     refs.startBtn.disabled = true;
+    refs.stopBtn.disabled = false;
     timerId = setInterval(() => {
         refs.bodyEl.style.backgroundColor = getRandomHexColor();
-    }, 1000);
+    }, 1000); 
 };
 
 function onStop() {
     clearInterval(timerId);
     refs.startBtn.disabled = false;
+    refs.stopBtn.disabled = true;
 };
 
