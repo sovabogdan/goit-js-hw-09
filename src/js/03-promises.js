804e38a09550b74e5f8611044c0145b621519184
Bohdan Sova <sova.bogdan.94@gmail.com>
2022-05-17 19:14:47 +0200

    update_task_03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,31 @@
+ 
+const refs = {
+  formEl: document.querySelector('.form'),
+  delayEl: document.querySelector('input[name="delay"]'),
+  stepEl: document.querySelector('input[name="step"]'),
+  amountEl: document.querySelector('input[name="amount"]'),
+  btnEl: document.querySelector('button[type="submit"]'),
+};
+
+refs.formEl.addEventListener('click', onPromises);
+
+function onPromises() {
+  promisesLoop();
+ };
+function promisesLoop() {
+  for (let position = 1; position <= refs.amountEl.value; position += 1){
+    createPromise();
+  };
+  
+ };
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
+  return new Promise((resolve, reject) => {
+    if (shouldResolve) {
+      resolve = console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
   } else {
-    // Reject
+    reject = console.log(`❌ Rejected promise ${position} in ${delay}ms`);
   }
-}
+  })
+  
+};
